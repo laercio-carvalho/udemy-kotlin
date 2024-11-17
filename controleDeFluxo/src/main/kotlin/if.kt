3d0c8d5320144d5dcaf591bb47351af24d2047fd
laercio-carvalho <laercio_ti-profissional@yahoo.com.br>
2024-11-17 19:49:32 -0300

    Learn: Aprendendo a utilizar Operador Ternário com Elvis Operator em condicionais if-else. Fiz tambem, nas ultimas linhas, uma extrapolacao para ver se funcionava encadear varios em uma unica linha.

--- controleDeFluxo/src/main/kotlin/if.kt
+++ controleDeFluxo/src/main/kotlin/if.kt
@@ -1,7 +1,9 @@
 fun main() {
     maiorDeIdade(70)
     println(saudacao(true))
-
+    println("Aceita bebida alcólica? \n${aceitaBebidaAlcoolica(false)}")
+    aceitaCha(true)
+    informarIdade(12)
 }
 
 fun maiorDeIdade(idade: Int){
@@ -13,15 +15,21 @@ fun maiorDeIdade(idade: Int){
     }else if (idade < 18){
         println("Menor de Idade")
     }else{
-        println("Crança")
+        println("Criança")
     }
 }
 
 fun saudacao(dia: Boolean): String {
-
     return if(dia){
         "Bom dia"
     }else{
         "Boa Noite"
     }
 }
+//utilizando Operador Ternário com Elvis Operator
+fun aceitaBebidaAlcoolica(respostaSobreBebida: Boolean = true): String {
+    return if (respostaSobreBebida) "Sim" else "não"
+}
+fun aceitaCha(respostaSobreCha:Boolean = false) = if (respostaSobreCha) println("Aceito") else println("Não Aceito")
+//Aqui é um teste de extrapolação em linha unica.
+fun informarIdade(informeIdade: Int) = if (informeIdade >= 18 && informeIdade <= 64) println("Maior de idade") else if (informeIdade >= 65 && informeIdade <= 100) println("Idoso") else if(informeIdade > 99) println("Quase Morto") else println("Menor de idade")
